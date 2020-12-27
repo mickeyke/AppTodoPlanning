cb861a3234a5ddbe7e36fe9bf9f1270983937c8f
Mickeyke <michael.carels@live.be>
2020-12-27 20:30:24 +0100

    Fragments aan gemaakt

--- app/src/main/java/com/ehb/apptodoplanning/MainActivity.java
+++ app/src/main/java/com/ehb/apptodoplanning/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ehb.apptodoplanning;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //create obtion menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);//resource omzetten naar scherm
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_add://uitvoeren als men op de add knop heeft geklikt , R. = resource
+                Toast.makeText(this,"Pressed add", Toast.LENGTH_LONG).show(); //Toast is een pop up scherm
+                //ga naar de insert naviagatie pagina, voor een nieuwe item.
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
